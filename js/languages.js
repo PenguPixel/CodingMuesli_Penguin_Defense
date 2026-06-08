f4b2265e2a2f0d9f51b6443db559b3d99b5ae73d
CodingMuesli <codingmuesli@gmail.com>
2026-06-08 10:38:14 +0200

    initial commit

---
+++ js/languages.js
@@ -0,0 +1,115 @@
+"use strict";
+
+class Languages {
+	static AVAILABLE_LANGUAGES = [ "German", "English" ];
+	static language = "English";
+	
+	static Attributes = {
+		Range:		Symbol("Range"),
+		AtkSpeed:	Symbol("AtkSpeed"),
+		StatName:	Symbol("StatName"),
+		Amount:		Symbol("Amount")
+	}
+	
+	static CardTexts = {
+		"German":	{
+			"Tower":	["Turm"],
+			"Range":	["Reichweite: ", this.Attributes.Range],
+			"AtkSpeed":	["Geschwindigkeit: ", this.Attributes.AtkSpeed],
+			"Upgrade":	["Upgrade"],
+			"Increase":	["Erhöhe die ", this.Attributes.StatName],
+			"By":		["um ", this.Attributes.Amount]
+		},
+		"English":	{
+			"Tower":	["Tower"],
+			"Range":	["Range: ", this.Attributes.Range],
+			"AtkSpeed":	["Attack Speed: ", this.Attributes.AtkSpeed],
+			"Upgrade":	["Upgrade"],
+			"Increase":	["Increase the ", this.Attributes.StatName],
+			"By":		["by ", this.Attributes.Amount]
+		}
+	};
+	
+	static _getAttributeValue(card, symbol) {
+		switch(symbol) {
+			case this.Attributes.Range: return card.data.range;
+			case this.Attributes.AtkSpeed: return card.data.attackSpeed;
+			case this.Attributes.StatName: return card.data.statName;
+			case this.Attributes.Amount: return card.data.amount;
+			default: return "null";
+		};
+	}
+	
+	static _assembleSingleText(card, dict, text) {
+		let snippets = dict[text];
+		let res = "";
+		for(let element of snippets) {
+			if(typeof element == "string") {
+				res += element;
+			} else {
+				res += Languages._getAttributeValue(card, element);
+			}
+		}
+		return res;
+	}
+	
+	static getCardDescByLanguage(card) {
+		let dict = Languages.CardTexts[Languages.language];
+		let texts = [];
+		if(card.type == "tower") {
+			texts.push(this._assembleSingleText(card, dict, "Tower"));
+			texts.push(this._assembleSingleText(card, dict, "Range"));
+			texts.push(this._assembleSingleText(card, dict, "AtkSpeed"));
+		} else if(card.type == "upgrade") {
+			texts.push(this._assembleSingleText(card, dict, "Upgrade"));
+			texts.push(this._assembleSingleText(card, dict, "Increase"));
+			texts.push(this._assembleSingleText(card, dict, "By"));
+		} else {
+			console.log("Invalid or unimplemented card type:");
+			console.log(card);
+		}
+		return texts;
+	}
+	
+	static _BUTTON_IDS = {
+		"German": {
+			"Levels":	"LEVELS_BUTTON",
+			"Play":		"PLAY_LEVEL_BUTTON",
+			"Continue":	"CONTINUE_BUTTON_GER",
+			"MainMenu":	"MAIN_MENU_BUTTON_GER",
+			"Reroll":	"REROLL_BUTTON_GER"
+		},
+		"English": {
+			"Levels":	"LEVELS_BUTTON",
+			"Play":		"PLAY_LEVEL_BUTTON",
+			"Continue":	"CONTINUE_BUTTON_EN",
+			"MainMenu":	"MAIN_MENU_BUTTON_EN",
+			"Reroll":	"REROLL_BUTTON_EN"
+		}
+	}
+	
+	static button(buttonTag) {
+		return Languages._BUTTON_IDS[Languages.language][buttonTag];
+	}
+	
+	static get START_MESSAGE() {
+		return {
+			"German":	"Klicke zum Starten",
+			"English":	"Click to Start"
+		}[Languages.language];
+	}
+	
+	static _STAT_SUMMARY_DICT = {
+		"German":	{
+			"Range":	"Reichw.: ",
+			"Speed":	"Geschw.: "
+		},
+		"English":	{
+			"Range":	"Range: ",
+			"Speed":	"Speed: "
+		}
+	}
+	static statSummary(stat) {
+		return Languages._STAT_SUMMARY_DICT[Languages.language][stat];
+	}
+}
---
+++ js/ui/Cards.js
@@ -0,0 +1,194 @@
+// Author: Philipp Locher
+// Additions: Timo Lauterbach (marked)
+class Card{
+	/*static RANGE_DESC = "Erhöht die Angriffsreichweite";
+	static SPEED_DESC = "Erhöht die Angriffsgeschwindigkeit";
+	static TROUTERANG_DESC = "Erhöht die Anzahl an Forellerängen für den Turm um 1.";
+	static PIERCE_DESC = "Erhöht die Anzahl an Gegnern, die ein Zapfen durchdringen kann.";*/
+	static _DESCS = {
+		"German":	{
+			"RangeDesc":		"Erhöht die Angriffsreichweite",
+			"SpeedDesc":		"Erhöht die Angriffsgeschwindigkeit",
+			"TrouterangDesc":	"Erhöht die Anzahl an Forellerängen für den Turm um 1.",
+			"PierceDesc":		"Erhöht die Anzahl an Gegnern, die ein Zapfen durchdringen kann.",
+			"RangeStatName":		"Reichweite",
+			"SpeedStatName":		"Geschwindigkeit",
+			"TrouterangStatName":	"Forellerang Anzahl",
+			"PierceStatName":		"Durchschuss",
+			"RangeSmall":		"Geringe Reichweite",
+			"RangeEpic":		"Große Reichweite",
+			"SpeedSmall":		"Geringe Geschwindigkeit",
+			"SpeedEpic":		"Große Geschwindigkeit",
+			"Trouterang":		"Forellerang",
+			"Pierce":			"Durchschuss"
+		},
+		"English":	{
+			"RangeDesc":		"Increases the range.",
+			"SpeedDesc":		"Increases the attack speed.",
+			"TrouterangDesc":	"Increases the amount of Trouterangs for this tower by 1.",
+			"PierceDesc":		"Increases the amount of pierce for this tower.",
+			"RangeStatName":		"Range",
+			"SpeedStatName":		"Attack Speed",
+			"TrouterangStatName":	"Trouterang Amount",
+			"PierceStatName":		"Pierce",
+			"RangeSmall":		"Little Range",
+			"RangeEpic":		"Big Range",
+			"SpeedSmall":		"Little Speedup",
+			"SpeedEpic":		"Big Speedup",
+			"Trouterang":		"Trouterang",
+			"Pierce":			"Pierce"
+		}
+	}
+	static get RANGE_DESC()		{ return Card._DESCS[Languages.language]["RangeDesc"]; }
+	static get SPEED_DESC() 	{ return Card._DESCS[Languages.language]["SpeedDesc"]; }
+	static get TROUTERANG_DESC(){ return Card._DESCS[Languages.language]["TrouterangDesc"]; }
+	static get PIERCE_DESC()	{ return Card._DESCS[Languages.language]["PierceDesc"]; }
+	static get DICT() { return Card._DESCS[Languages.language]; }
+	
+	id;			// unique name
+	type;		// tower / upgrade / etc.
+	name;		// shown name
+	data;		// stats and effects
+	img;		// img from html
+	rate;
+	
+	constructor(id, type, name, data, img, rate){
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.data = data;
+		this.img = img;
+		this.rate = rate;
+	}
+}
+
+class CardRegistry {
+	static classes = [];
+	
+	static register(cardClass) {
+		CardRegistry.classes.push(cardClass);
+	}
+}
+
+class CardPool{ 
+	static cards = {};
+	
+	static load() {
+		CardPool.cards = [];
+		
+		for (let CardClass of CardRegistry.classes) {
+			CardPool.register(new CardClass());
+		}
+		
+	}
+	
+	static register(card){
+		CardPool.cards[card.id] = card;
+	}
+	
+	static get(id){
+		return CardPool.cards[id] || null;
+	}
+	
+	static createDeck(){
+		let deck = [];
+		
+		for (let card of Object.values(CardPool.cards)){
+			deck.push(card);
+		}
+		return deck;
+	}
+	
+	static weightedRandom(list) {			// includes drop rate from card object
+		let totalRate = 0;
+		
+		for (let i = 0; i < list.length; i++){
+			totalRate += list[i].rate;
+		}
+		
+		let random =Math.random() * totalRate;
+		for (let i = 0; i < list.length; i++){
+			random -= list[i].rate;
+			
+			if (random <= 0) {	return list[i];	}
+		}
+		return list[list.length -1];
+	}
+	
+	static removeCardOnce(list, card) {		// prevents cards from being drawn multiple times
+		for (let i = 0; i < list.length; i++){
+			if (list[i] === card) {
+				list.splice(i,1);
+				return;
+			}
+		}
+	}
+	
+	static createThreeCardChoice(deck, towers, money){
+		let result = [];
+		let pool = deck.slice();
+
+		let allTowers = [];
+		for (let i = 0; i < pool.length; i++){
+			if (pool[i].type === "tower") {
+				allTowers.push(pool[i]);
+			}
+			// Timo: remove unusable cards from pool	------------------------------------------------------------------------------------------------------------
+			if(pool[i].type === "upgrade") {
+				let towerInGame = false;
+				for(let tower of towers) {
+					if(tower instanceof pool[i].data.towerType) {
+						towerInGame = true;
+						break;
+					}
+				}
+				if(!towerInGame) {
+					pool.splice(i--, 1);
+				}
+			}
+			//	---------------------------------------------------------------------------------------------------------------------------------------------------
+		}
+		
+		if(pool.length === 0) return result;
+		
+		// always get one tower
+		if (allTowers.length > 0) {
+			let towerCard = CardPool.weightedRandom(allTowers);
+			result.push(towerCard);
+			CardPool.removeCardOnce(pool, towerCard);
+		}
+	
+		// get cheapest remaining card from pool
+		let cheapestCost = Infinity;
+		for (let card of pool) {
+			if (card.data.cost < cheapestCost) {
+				cheapestCost = card.data.cost;
+			}
+		}
+	
+		// show at least one affordable card as long as player has enough currency for cheapest available card
+		if (money >= cheapestCost) {
+			let affordableCards = [];
+	
+			for (let card of pool) {
+				if (card.data.cost <= money) {
+					affordableCards.push(card);
+				}
+			}
+	
+			if (affordableCards.length > 0) {
+				let safeAffordable = CardPool.weightedRandom(affordableCards);
+				result.push(safeAffordable);
+				CardPool.removeCardOnce(pool, safeAffordable);
+			}
+		}
+	
+		while (result.length < 3 && pool.length > 0) {
+			let picked = CardPool.weightedRandom(pool);
+			result.push(picked);
+			CardPool.removeCardOnce(pool, picked);
+		}
+	
+		return result;
+	}
+}
